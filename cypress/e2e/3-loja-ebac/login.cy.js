6d1a946538a239396b514dcdde5e9f21a9ad08cc
Cassia Mesquita <cassiamesquita89@gmail.com>
2024-06-07 13:02:34 -0300

    primeiro teste automatizado

---
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -0,0 +1,11 @@
+///<Reference types="cypress"/>
+
+describe('funcionalidade: login', () => {
+
+    it('Deve fazer login com sucesso', ()=>{
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('cassia.teste@teste.com.br')
+        cy.get('#password').type('teste@123')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, cassia.teste (não é cassia.teste? Sair)')    })
+})
